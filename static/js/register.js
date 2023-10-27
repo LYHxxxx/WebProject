390d0b00f750e039db0920cd0a575a7f32f086e4
LYHxxxx <130650577+LYHxxxx@users.noreply.github.com>
2023-10-27 13:22:13 +0800

    Add files via upload

---
+++ static/js/register.js
@@ -0,0 +1,18 @@
+// 如果验证不通过禁止提交表单
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // 获取表单验证样式
+      var forms = document.getElementsByClassName('needs-validation');
+      // 循环并禁止提交
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
